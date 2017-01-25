79dad186451e229064244ab1e042b8512a63679d
Sigurdur Axel <Sigurdurar@nemi.tskoli.is>
2017-01-25 08:53:18 +0000

    daemi 3

--- foritun.py
+++ foritun.py
@@ -1,8 +1,8 @@
 #Sigurður Axel
 #25.1.2017
 #Dæmi 1
-tala 1 = int(input("Sláðu inn fyrstu tölu"))
-tala 2 = int(input("Sláðu inn seinni tölu"))
+tala1 = int(input("Sláðu inn fyrstu tölu"))
+tala2 = int(input("Sláðu inn seinni tölu"))
 summa = tala1 + tala2
 marg = tala1 * tala2
 print("Tölurnar lagðar saman:" , summa , "og margfaldaðar saman:" , marg)
@@ -11,3 +11,14 @@ print("Tölurnar lagðar saman:" , summa , "og margfaldaðar saman:" , marg)
 fornafn = input("Sláðu inn fornafn þitt ")
 eftirnafn = input("Sláðu inn eftir nafn þitt ")
 print("Halló" , fornafn , eftirnafn)
+
+#Dæmi 3
+ha = 0
+la = 0
+setning = input("Sláðu inn texta ")
+for o in setning:
+    if o.isupper():
+        ha = ha +1
+    else:
+        la = la +1
+print("Í þessum texta eru," , ha , "hástafir" , la , "lágstafir")
