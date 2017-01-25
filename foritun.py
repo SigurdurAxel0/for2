a33e313b6a899a4ff359ba4ea019b2019660596f
Sigurdur Axel <Sigurdurar@nemi.tskoli.is>
2017-01-25 08:43:55 +0000

    daemi 1

--- foritun.py
+++ foritun.py
@@ -1,2 +1,8 @@
-#sigur�ur
-print("halló heimur")
+#Sigurður Axel
+#25.1.2017
+#Dæmi 1
+tala 1 = int(input("Sláðu inn fyrstu tölu"))
+tala 2 = int(input("Sláðu inn seinni tölu"))
+summa = tala1 + tala2
+marg = tala1 * tala2
+print("Tölurnar lagðar saman:" , summa , "og margfaldaðar saman:" , marg)
